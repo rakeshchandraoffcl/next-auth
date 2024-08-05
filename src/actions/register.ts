7fb2998bb54e8d0e5cca92806a61f2991e5a8be7
rakesh <rakeshchandra.offcl@gmail.com>
2024-08-06 00:07:13 +0530

    prisma set up

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,10 +1,13 @@
 "use server";
 
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
 import * as z from "zod";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
 	const validatedValues = registerSchema.safeParse(values);
+	const users = await db.user.findMany();
+	console.log({ users });
 	if (!validatedValues.success) {
 		throw new Error("Invalid fields");
 	}
